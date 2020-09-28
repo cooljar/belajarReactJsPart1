34b801fb697434c6287b85747acbf6336dbdf183
cooljar <lifelinejar@gmail.com>
2020-09-29 00:29:08 +0700

    no eff

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,6 +3,50 @@ import React from 'react';
 class Table extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            provinsis: [
+                {
+                    id: '18',
+                    provinsi: 'Lampung'
+                }
+            ],
+            addedProvinsi: {id: '18', provinsi: 'Lampung'},
+            newId: "",
+            newProvinsi: ""
+        };
+
+        // Binding method
+        this.setId = this.setId.bind(this);
+        this.setProvinsi = this.setProvinsi.bind(this);
+        this.tambahData = this.tambahData.bind(this);
+    }
+
+    renderTableData() {
+        return this.state.provinsis.map((provinsiData, index) => {
+            const { id, provinsi } = provinsiData //destructuring
+            return (
+                <tr key={id}>
+                    <td>{id}</td>
+                    <td>{provinsi}</td>
+                </tr>
+            )
+        })
+    }
+
+    setId(e){
+        this.setState({newId: e.target.value});
+    }
+
+    setProvinsi(e){
+        this.setState({newProvinsi: e.target.value});
+    }
+
+    tambahData(){
+        let sts = this.state.provinsis;
+        let st = {"id":this.state.newId, "provinsi":this.state.newProvinsi};
+        sts.push(st);
+        this.setState({provinsis: sts});
+        this.setState({addedProvinsi: st});
     }
 
     render() {
@@ -16,22 +60,22 @@ class Table extends React.Component {
                     </tr>
                 </thead>
                 <tbody>
-                    
+                    {this.renderTableData()}
                 </tbody>
             </table>
 
             <div style={styles.container}>
-            ID: <input type="text" name="id"></input>
+            ID: <input type="text" onChange={this.setId} name="id"></input>
             <span>&nbsp;</span> <span>&nbsp;</span>
-            PROVINSI: <input type="text" name="instansi"></input>
+            PROVINSI: <input type="text" onChange={this.setProvinsi} name="provinsi"></input>
             <span>&nbsp;</span>
-            <button>TAMBAH</button>
+            <button onClick={this.tambahData}>TAMBAH</button>
             </div>
 
             <div style={styles.container}>
             Baru Saja ditambahkan:
-            <br></br>ID: <span></span>
-            <br></br>PROVINSI: <span></span>
+            <br></br>ID: <span>{this.state.addedProvinsi.id}</span>
+            <br></br>provinsi: <span>{this.state.addedProvinsi.provinsi}</span>
             </div>
         </div>;
     }
